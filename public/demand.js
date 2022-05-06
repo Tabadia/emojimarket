6649680997a64fa6ae86cea86a40a6d127a435b7
Thalen <tabadia7574@student.pps.net>
2022-05-05 20:06:58 -0700

    updated trophies, emojis, and messing around with graphs

--- public/demand.js
+++ public/demand.js
@@ -1,31 +1,75 @@
 var xValues = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-var yValues = Array.from({length: 30}, () => Math.floor(Math.random() * 12));
+var yValues = Array.from({length: 30}, () => Math.floor(Math.random() * 100));
+var lineValues = Array.from({length: 30}, () => Math.floor(Math.random() * 100));
+var pieValues = [1,2,3,4,5]
 
-new Chart("myChart", {
-  type: "line",
+const barChart = new Chart("myChart", {
+  type: "bar",
   data: {
     labels: xValues,
-    datasets: [{
+    
+    datasets: [
+      {
+        label: 'Average',
+        type: 'line',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,0,1.0)",
+        borderColor: "rgba(0,0,0,0.7)",
+        data: lineValues,
+        stack: 'combined',
+      },
+      {
+      label: 'Emoji',
       fill: true,
       lineTension: 0,
       backgroundColor: "rgba(255,255,255,1.0)",
       borderColor: "rgba(255,255,255,0.5)",
-      data: yValues
-    },
-]
+      data: yValues,
+      stack: 'combined',
+    },]
   },
 
   options: {
-    legend: {display: false},
+    legend: {},
     scales: {
         yAxes: [{
             scaleLabel: {
                 display: true,
-                labelString: 'Trades'}
+                labelString: 'Trades'},
+                min: 0,
+                suggestedMax: 35,
             },],
         xAxes: [{
             scaleLabel: {
                 display: true,
                 labelString: 'Days',}
             },],}
-        }})
+        }
+})
+
+new Chart("pieChart", {
+  type: "pie",
+  data: {
+    labels: ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary'],   
+    datasets: [
+      {
+        type: 'pie',
+
+        data: pieValues,
+      },]
+  },
+
+  options: {
+  }
+})
+
+
+function select() {
+  var time = document.getElementById('ti');
+  console.log(time.value)
+  if (time.value = "7") {
+    xValues = [1,2,3,4,5,6,7];
+    barChart.update();
+  }
+}
