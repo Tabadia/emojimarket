47b5eb2559697515c5ae69f7eb2a943d88836007
Thalen <tabadia7574@student.pps.net>
2022-05-18 22:37:53 -0700

    fixing rarity search

--- docs/database.js
+++ docs/database.js
@@ -8,32 +8,34 @@ legendary = document.getElementById('legendary').innerHTML;
 function search(){
     input = document.getElementById('input').value;
     input = input.substring(input.length - 1);
+    document.getElementById('input').value = input;
     input = input + " ";
     console.log(input);
 
-    if(input.length = 0){
+    if(input.length <= 0){
         document.getElementById('result').innerHTML = "Result"
     }
 
-    else if(common.includes(input)) {
-        document.getElementById('result').innerHTML = 'Common';
+    else if(legendary.includes(input)) {
+        document.getElementById('result').innerHTML = 'Legendary';
+    }
+
+    else if(epic.includes(input)) {
+        document.getElementById('result').innerHTML = 'Epic';
+    }
+
+    else if(rare.includes(input)) {
+        document.getElementById('result').innerHTML = 'Rare';
     }
 
     else if(uncommon.includes(input)) {
         document.getElementById('result').innerHTML = 'Uncommon';
     }
     
-    else if(rare.includes(input)) {
-        document.getElementById('result').innerHTML = 'Rare';
+    else if(common.includes(input)) {
+        document.getElementById('result').innerHTML = 'Common';
     }
     
-    else if(epic.includes(input)) {
-        document.getElementById('result').innerHTML = 'Epic';
-    }
-    
-    else if(legendary.includes(input)) {
-        document.getElementById('result').innerHTML = 'Legendary';
-    }
 
     else{
         document.getElementById('result').innerHTML = "Emoji not found"
