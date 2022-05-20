26b4d24c84bfe94e18874252cbc2b91844f203c8
Thalen <tabadia7574@student.pps.net>
2022-05-19 21:45:28 -0700

    trophy search result number fixed

--- docs/trophy.js
+++ docs/trophy.js
@@ -1,19 +1,24 @@
 var trophy = document.getElementsByClassName("trophy");
 
 function search(){
+    var count = 0;
     input = document.getElementById('input').value;
     input = input.toLowerCase();
+    results = document.getElementById('result').innerHTML;
     for (var i = 0; i < trophy.length; i++) {
         var trophyTitle = trophy.item(i).innerHTML.toLowerCase();
+        trophyTitle = trophyTitle.replace('<h1 class="trophytitle">', '').replace('<h1 class="trophytxt">', '').replaceAll('</h1>', '')
         var trophyClass = trophy.item(i).classList;
         if(trophyTitle.includes(input)){
             if(trophyClass.toString().includes("hidden")){
                 trophyClass.remove("hidden");
             }
+            count++;
             console.log(trophyTitle);
         }
         else{
             trophyClass.add("hidden");
         }
+        document.getElementById('result').innerHTML = count;
      }
 }
